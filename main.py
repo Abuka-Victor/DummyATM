420e608aabcff023b6aab3f5691f4298ab60e9bb
Abuka-Victor <abukavictoro@gmail.com>
2021-06-13 00:33:48 +0100

    Did Some Code on the GUI.

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
 import database
+from tkinter import *
 
+window = Tk()
+window.title("Central Banque Of Nigeria")
+window.geometry("1000x500")
+
+header = Frame()
+footer = Frame()
+aside = Frame()
+body = Frame()
+wl = Label(text='Welcome to the Central Banque of Nigeria')
+wl.grid(row=0, column=0)
+
+
+
+
+
+
+
+
+
+window.mainloop()
