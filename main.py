33d17ecaebe878af8a710fad489baed2cc3d84c8
Abuka-Victor <abukavictoro@gmail.com>
2021-06-14 01:42:03 +0100

    New Picture File, Used it as a background. I also downloaded the PIL library so i can use the photo as a background without stress. Added a button as well.

--- main.py
+++ main.py
@@ -1,25 +1,33 @@
 import database
 from tkinter import *
+from PIL import ImageTk
 
 window = Tk()
 window.title("Central Banque Of Nigeria")
-window.geometry("1200x700")
+window.geometry("1000x667")
+window.resizable(False,False)
 
+bgimg = ImageTk.PhotoImage(file="bankappbg.jpg")
+label = Label(window, image=bgimg)
+label.pack()
 
 body = Frame(window)
-body.place(x=390, y=170, width=400, height=450)
+body.place(x=290, y=170, width=400, height=450)
 
 accno_label = Label(body, text='Account Number', font=("Andalusia", 15, 'bold'))
-accno_label.place(x='80', y='50')
+accno_label.place(x='100', y='120')
 
 accno_entry = Entry(body, font=("Times New Roman", 15))
-accno_entry.place(x="80", y="80", width='190')
+accno_entry.place(x="100", y="150", width='190')
 
 accpin_label = Label(body, text='PIN', font=("Andalusia", 15, 'bold'))
-accpin_label.place(x='290', y='50')
+accpin_label.place(x='100', y='200')
 
-accpin_entry = Entry(body, font=("Times New Roman", 15))
-accpin_entry.place(x="290", y="80", width='190')
+accpin_entry = Entry(body, show="*", font=("Times New Roman", 15))
+accpin_entry.place(x="100", y="230", width='190')
+
+submit_button = Button(body, text="Submit")
+submit_button.place(x="165", y="280")
 
 
 
