778e82eadf07ff26bb49643d06e6b5c9ab6583f8
Abuka-Victor <abukavictoro@gmail.com>
2021-06-14 01:15:46 +0100

    Added Acc No field and Pin Field

--- main.py
+++ main.py
@@ -3,19 +3,23 @@ from tkinter import *
 
 window = Tk()
 window.title("Central Banque Of Nigeria")
-window.geometry("1000x500")
-
-header = Frame()
-footer = Frame()
-aside = Frame()
-body = Frame()
-wl = Label(text='Welcome to the Central Banque of Nigeria')
-wl.grid(row=0, column=0)
+window.geometry("1200x700")
 
 
+body = Frame(window)
+body.place(x=390, y=170, width=400, height=450)
 
+accno_label = Label(body, text='Account Number', font=("Andalusia", 15, 'bold'))
+accno_label.place(x='80', y='50')
 
+accno_entry = Entry(body, font=("Times New Roman", 15))
+accno_entry.place(x="80", y="80", width='190')
 
+accpin_label = Label(body, text='PIN', font=("Andalusia", 15, 'bold'))
+accpin_label.place(x='290', y='50')
+
+accpin_entry = Entry(body, font=("Times New Roman", 15))
+accpin_entry.place(x="290", y="80", width='190')
 
 
 
